47ab533cd922f5f4dd856d581cfd906e220ee431
Kane Mathis <admin@kanemathis.com>
2022-09-07 20:09:54 -0400

    commit tree 8bfea8f33a07c29fb115006649a5fe904d7688d9

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_reader:brand
+    attr_accessor:size
+    attr_accessor:material
+    attr_accessor:condition
+    attr_accessor:color
+    def initialize(brand)
+        @brand=brand
+    end
+    def cobble
+        self.condition="new"
+        puts "Your shoe is as good as new!"
+    end
+end
+
+myshoe=Shoe.new("Nike")
+myshoe.condition="old"
+puts myshoe.cobble
